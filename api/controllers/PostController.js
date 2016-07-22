78fd903291842b716c978d0a5f2474bf12f0cd47
OOR <yui12327@gmail.com>
2016-07-23 03:06:58 +0800

    add upload img post

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -1,12 +1,30 @@
+const path = require('path');
+
 module.exports = {
   addPost: async (req, res) => {
     try {
       // 從前端 request 取得 form 表單內容
       let data = req.body;
       data.UserId = req.session.uid;
+      req.file('img').upload({
+        // don't allow the total upload size to exceed ~10MB
+        maxBytes: 10000000,
+        dirname: path.join('..', '..', '.tmp', 'public', 'uploads')
+      },function whenDone(err, uploadedFiles) {
+        if (err) {
+          return res.negotiate(err);
+        }
 
-      await PostService.addPost(data);
-      res.redirect('/');
+        if (uploadedFiles.length === 0){
+          data.img = undefined;
+        }else {
+          data.img = path.basename(uploadedFiles[0].fd)
+        }
+        PostService.addPost(data);
+        res.redirect('/');
+
+      });
+
     } catch (e) {
       res.serverError(e);
     }
